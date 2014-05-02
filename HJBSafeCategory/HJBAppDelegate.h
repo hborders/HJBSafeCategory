56975b2b3ffbe59420fc95edb0006c4a985dec73
Heath Borders <heath.borders@jivesoftware.com>
2014-05-01 22:16:06 -0500

    Initial Commit

---
+++ HJBSafeCategory/HJBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HJBAppDelegate.h
+//  HJBSafeCategory
+//
+//  Created by Heath Borders on 5/1/14.
+//  Copyright (c) 2014 Heath Borders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HJBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HJBSafeCategory/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HJBSafeCategory
+//
+//  Created by Heath Borders on 5/1/14.
+//  Copyright (c) 2014 Heath Borders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HJBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HJBAppDelegate class]));
+    }
+}
